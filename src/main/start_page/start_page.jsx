5c6fa99bd0c3da1f1429b6086e75c87c5cddbb26
Spudi <inf@gm.ru>
2022-06-17 14:28:40 +0300

    Верстка start_page

--- src/main/start_page/start_page.jsx
+++ src/main/start_page/start_page.jsx
@@ -1,9 +1,15 @@
+import DopInfo from './dop_info/dop_info';
+import News from './news/news';
+import Questions from './questions/questions';
 import s from './start_page.module.css'
 
 function StartPage() {
     return (
         <div>
-            start
+            <News />
+            <DopInfo />
+            <Questions />
+
         </div>
 
     )
